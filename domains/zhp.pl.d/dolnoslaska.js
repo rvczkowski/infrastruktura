6d5152e2f3c58f5299fe2b6746dd784c5b02f909
rvczkowski <59458392+rvczkowski@users.noreply.github.com>
2024-03-02 15:15:54 +0100

    Update dolnoslaska.js

--- domains/zhp.pl.d/dolnoslaska.js
+++ domains/zhp.pl.d/dolnoslaska.js
@@ -1,26 +1,14 @@
 var cyberfolksNs = ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.'];
 
-D_EXTEND('zhp.pl',
-
-    //Strona chorągwi     
+D_EXTEND('zhp.pl',  
     Ms365_Subdomain('dolnoslaska','zhp.pl'),
-    Delegation_A_WithCfProxy('dolnoslaska', '213.32.10.111'),
-    Delegation_NS('archiwumdolnoslaska', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']),     
-	Delegation_NS('dolnoslaska2', cyberfolksNs),
-	Delegation_NS('dolnoslaska3', cyberfolksNs),
-
-	// Bazy
-    Delegation_NS('czarna', cyberfolksNs),
-    Delegation_NS('czarna2', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
-    Delegation_NS('pobierowo', cyberfolksNs),
-    Delegation_NS('pobierowo2', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
-    Delegation_NS('rozewie', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']),
-
-    // Hufce
+	Ms365_Subdomain('glogow', 'zhp.pl'),
+	Ms365_Subdomain('milicz', 'zhp.pl'),
+    Delegation_A('dolnoslaska', '213.32.10.111'),
+	Delegation_A('glogow', '213.32.10.111'),
 	Delegation_NS('boleslawiec', ['ns1.cba.pl.', 'ns2.cba.pl.', 'ns3.cba.pl.']),
 	Delegation_NS('bystrzycaklodzka', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
     Delegation_NS('dzierzoniow', ['ns.lh.pl.', 'ns2.lighthosting.net.', 'ns2.lh.pl.']),
-  	Delegation_NS('glogow', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']),
 	Delegation_NS('jawor', ['dns11.linuxpl.com.', 'ns11.linuxpl.com.']),
 	Delegation_NS('karkonoski', ['ns1.ehost.pl.', 'ns2.ehost.pl.']),
 	Delegation_NS('klodzko', ['dns8.linuxpl.com.', 'ns8.linuxpl.com.']),
@@ -28,7 +16,6 @@ D_EXTEND('zhp.pl',
 	Delegation_NS('luban', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
 	Delegation_NS('lubin', ['ns1.hostdmk.net.', 'ns2.hostdmk.net.']),
 	Delegation_NS('lagiewniki', ['dns3.hitme.net.pl.', 'dns4.hitme.net.pl.']),
-	Ms365_Subdomain('milicz', 'zhp.pl'),
 	Delegation_NS('olesnica', ['ns1.datanet.pl.', 'ns2.datanet.pl.']),
 	Delegation_NS('olawa', ['ns12.linuxpl.com.', 'dns12.linuxpl.com.']),
     Delegation_NS('srodaslaska', ['dns8.linuxpl.com.', 'ns8.linuxpl.com.']),
@@ -39,7 +26,9 @@ D_EXTEND('zhp.pl',
 	Delegation_NS('wroclawpoludnie', ['ns.lh.pl.', 'ns2.lighthosting.net.', 'ns2.lh.pl.']),
 	Delegation_NS('wroclawwschod', ['ns1.hostido.net.pl.','ns2.hostido.net.pl.', 'ns3.hostido.net.pl.']),
     Delegation_NS('zmigrod', ['ns.lh.pl.', 'ns2.lighthosting.net.']),
-
     Delegation_NS('bumerang', ['ns.lh.pl.', 'ns2.lighthosting.net.', 'ns2.lh.pl.']),
-    Delegation_NS('rajdgranica', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.'])
+    Delegation_NS('rajdgranica', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']),
+    Delegation_NS('czarna', cyberfolksNs),
+    Delegation_NS('pobierowo', cyberfolksNs),
+    Delegation_NS('rozewie', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.'])
 );
